5065b8f909d229bf5cc0771ecbcc16047e06e02e
wnsdud4206 <wnsdud4206@gmail.com>
2020-10-22 15:48:19 +0900

    20201022_4week_homework

--- app.py
+++ app.py
@@ -17,36 +17,39 @@ def home():
 @app.route('/order', methods=['POST'])
 def post_article():
     # 1. 클라이언트로부터 데이터를 받기
-    url_receive = request.form["url_give"]
-    comment_receive = request.form["comment_give"]
+    un_receive = request.form["userName_give"]
+    cs_receive = request.form["color_select_give"]
+    st_receive = request.form["count_give"]
+    ar_receive = request.form["address_give"]
+    nb_receive = request.form["number_give"]
 
     # 2. meta tag를 스크래핑하기
     # url = 'https://platum.kr/archives/120958'     # 필요없음
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    # data = requests.get('localhost:5000', headers=headers)
+    #
+    # soup = BeautifulSoup(data.text, 'html.parser')
 
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    og_image = soup.select_one('meta[property="og:image"]')["content"]
-    og_title = soup.select_one('meta[property="og:title"]')["content"]
-    og_description = soup.select_one('meta[property="og:description"]')["content"]
+    # og_image = soup.select_one('meta[property="og:image"]')["content"]
+    # og_title = soup.select_one('meta[property="og:title"]')["content"]
+    # og_description = soup.select_one('meta[property="og:description"]')["content"]
 
     # 3. mongoDB에 데이터 넣기
 
     doc = {
-        "url": url_receive,
-        "comment": comment_receive,
-        "title": og_title,
-        "description": og_description,
-        "image": og_image
+        "userName": un_receive,
+        "color_select": cs_receive,
+        "count": st_receive,
+        "address": ar_receive,
+        "number": nb_receive
     }
 
     # print(doc)
     db.order.insert_one(doc)
 
-    return jsonify({'result': 'success', 'msg': 'POST 연결되었습니다!'})
+    return jsonify({'result': 'success', 'msg': '주문이 완료되었습니다.'})
 
 
 # 서버?
